47494f8014479bebd9304cc12f3ad3e0d9f2cc6f
xfdingustc <xfdingyy@qq.com>
2016-05-24 10:37:48 +0800

    add BaseActivity

--- app/src/main/java/xfdingustc/omegasupreme/MainActivity.java
+++ app/src/main/java/xfdingustc/omegasupreme/MainActivity.java
@@ -1,13 +1,27 @@
 package xfdingustc.omegasupreme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+
+    }
+
+    private void init() {
+        initViews();
+    }
+
+    private void initViews() {
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void setupToolbar() {
+        super.setupToolbar();
+        getToolbar().setTitle(R.string.app_name);
     }
 }
